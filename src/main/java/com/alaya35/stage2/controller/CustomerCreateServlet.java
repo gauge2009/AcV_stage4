144a048a04b93658c24cc5d9e66c45de50474773
gauge2009 <promvc@live.com>
2016-11-28 23:10:43 +0800

    stage2

---
+++ src/main/java/com/alaya35/stage2/controller/CustomerCreateServlet.java
@@ -0,0 +1,35 @@
+package com.alaya35.stage2.controller;
+
+/**
+ * Created by gauge on 2016/11/25.
+ */
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 创建客户
+ */
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends HttpServlet {
+
+    /**
+     * 进入 创建客户 界面
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO
+    }
+
+    /**
+     * 处理 创建客户 请求
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO
+    }
+}
